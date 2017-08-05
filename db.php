e80d282482fae36ac7aff740240b0f75a63ee82d
Naveen Jain <nveenjain@gmail.com>
2017-08-05 22:36:09 +0530

    Added login and register function

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+if(($_SERVER["REQUEST_URI"])=='/fundai/fundai/function.php'){
+	header("Location:index.php");
+}
+$host = 'localhost';
+$db   = '';//Database Name here
+$user = '';//Username here
+$pass = '';//Password here
+$charset = 'utf8';
+?>
